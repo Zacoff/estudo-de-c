34431d8838ca5b802d059f0c32f76358cf346101
Zacoff <daniel.dfa81@gmail.com>
2021-04-15 10:50:14 -0300

    AULA-1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int anoNascimento = 2001;
+    int anoAtual = 2021;
+
+    int resultado = (anoAtual - anoNascimento);
+
+    printf("Daniel tem %d anos", resultado); 
+
+    return 0;
+
+}
